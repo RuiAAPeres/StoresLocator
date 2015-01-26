71c73c0931a48c9a0d383c474a3826129450d605
Rui Peres <sauron.is.alive@gmail.com>
2015-01-26 23:14:56 +0000

    Added the map

---
+++ App Delegate/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Stores Locator
+//
+//  Created by Rui Peres on 26/01/2015.
+//  Copyright (c) 2015 Rui Peres. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
