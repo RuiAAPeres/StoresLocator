9b43218fd3932be189c4346ac4d154701d65e4dd
Rui Peres <sauron.is.alive@gmail.com>
2015-01-27 00:31:10 +0000

    Added the Location Manager + NSLocationWhenInUseUsageDescription

---
+++ View Controllers/StoresViewController.swift
@@ -0,0 +1,31 @@
+//
+//  StoresViewController.swift
+//  Stores Locator
+//
+//  Created by Rui Peres on 27/01/2015.
+//  Copyright (c) 2015 Rui Peres. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+import MapKit
+
+class StoresViewController : UIViewController {
+    
+    // MARK: Properties
+
+    let locationManager : CLLocationManager
+
+    // MARK: UIViewController Overrides
+
+    required init(coder aDecoder: NSCoder) {
+        locationManager = CLLocationManager()
+        super.init(coder: aDecoder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        locationManager.requestWhenInUseAuthorization()
+    }
+    
+}
