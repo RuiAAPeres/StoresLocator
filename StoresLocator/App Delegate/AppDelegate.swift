ae97a7cf077d2b014049f959130f2791590818a4
Rui Peres <sauron.is.alive@gmail.com>
2015-01-27 23:26:53 +0000

    Fixed the project + Added Parse Keys

--- StoresLocator/App Delegate/AppDelegate.swift
+++ StoresLocator/App Delegate/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
                 
-        Parse.setApplicationId("YOUR_APPLICATION_ID", clientKey: "YOUR_CLIENT_KEY")
+        Parse.setApplicationId(PARSE_APPLICATION_ID, clientKey: PARSE_CLIENT_KEY)
         PFAnalytics.trackAppOpenedWithLaunchOptionsInBackground(launchOptions, block: nil)
         
         return true
--- StoresLocator/View Controllers/StoresViewController.swift
+++ StoresLocator/View Controllers/StoresViewController.swift
@@ -31,7 +31,7 @@ class StoresViewController : UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         locationManager.requestWhenInUseAuthorization()
-        
+                
         setupToolBarIcons()
     }
     
